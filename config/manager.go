f1ae18ea257089d8994d07bce01e7961e865e58e
Yang Guo <gy@fir.im>
2018-12-25 16:02:04 +0800

    mock agent connect response data

--- config/manager.go
+++ config/manager.go
@@ -28,14 +28,18 @@ func (m *Manager) Connect() (domain.Settings, error) {
 	body, _ := json.Marshal(domain.AgentConnect{Token: token, Port: port})
 
 	var message domain.SettingsResponse
-	resp, err := http.Post(uri, "application/json", bytes.NewBuffer(body))
-	if err != nil {
+	resp, errFromReq := http.Post(uri, "application/json", bytes.NewBuffer(body))
+	if errFromReq != nil {
 		return message.Data, fmt.Errorf(ErrSettingConnectFail)
 	}
 
 	defer resp.Body.Close()
 	raw, _ := ioutil.ReadAll(resp.Body)
-	json.Unmarshal(raw, &message)
+	errFromJSON := json.Unmarshal(raw, &message)
+
+	if errFromJSON != nil {
+		return message.Data, errFromJSON
+	}
 
 	if !message.IsOk() {
 		return message.Data, fmt.Errorf(message.Message)
--- config/manager_test.go
+++ config/manager_test.go
@@ -1,24 +1,69 @@
 package config
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	rBody = []byte(`{
+		"code": 200,
+		"message": "mock",
+		"data": {
+			"agent": {
+				"id": "1",
+				"name": "local",
+				"token": "xxx-xxx",
+				"host": "test",
+				"tags": ["ios", "mac"],
+				"status": "OFFLINE",
+				"jobid": "job-id"
+			},
+	
+			"queue": {
+				"host": "127.0.0.1",
+				"port": 15671,
+				"username": "guest",
+				"password": "guest"
+			},
+	
+			"zookeeper": {
+				"host": "127.0.0.1:2181",
+				"root": "/flow-x"
+			},
+	
+			"callbackQueueName": "callback-q",
+			"logsExchangeName": "logs-exchange"
+		}
+	}`)
+
+	ts = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path == "/agents/connect" {
+			w.Write(rBody)
+		}
+	}))
+)
+
 func init() {
-	os.Setenv("FLOWCI_SERVER_URL", "http://localhost:8080")
+	os.Setenv("FLOWCI_SERVER_URL", ts.URL)
 	os.Setenv("FLOWCI_AGENT_TOKEN", "ca9b8be2-c0e5-4b86-8fdc-b92d921597a0")
 	os.Setenv("FLOWCI_AGENT_PORT", "8081")
 }
 
 func TestShouldConnectServerAndGetSettings(t *testing.T) {
 	assert := assert.New(t)
+	defer ts.Close()
 
 	configManager := Manager{}
 	settings, err := configManager.Connect()
 
 	assert.Nil(err)
 	assert.NotNil(settings)
+
+	assert.Equal("1", settings.Agent.ID)
+	assert.Equal("xxx-xxx", settings.Agent.Token)
 }
