6f105c34a293eab36e8593a5cabd70857ff75c4b
Yang Guo <gy@fir.im>
2019-05-23 00:57:39 +0200

    upload cmd log

--- service/log_consumer.go
+++ service/log_consumer.go
@@ -3,7 +3,10 @@ package service
 import (
 	"bufio"
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"mime/multipart"
 	"net/http"
 	"os"
@@ -32,7 +35,7 @@ func logConsumer(cmd *domain.CmdIn, channel <-chan *domain.LogItem) {
 	for {
 		item, ok := <-channel
 		if !ok {
-			return
+			break
 		}
 
 		util.LogDebug(item.Content)
@@ -45,33 +48,38 @@ func logConsumer(cmd *domain.CmdIn, channel <-chan *domain.LogItem) {
 			writeLogToQueue(exchangeName, channel, item)
 		}
 	}
+
+	err := uploadLog(cmd)
+	util.LogIfError(err)
 }
 
-func uploadLog(cmd *domain.ExecutedCmd) error {
+func uploadLog(cmd *domain.CmdIn) error {
 	config := config.GetInstance()
 	logFile := filepath.Join(config.LoggingDir, cmd.ID+".log")
 
 	// read file
 	file, err := os.Open(logFile)
-	if err != nil {
+	if util.HasError(err) {
 		return err
 	}
-
 	defer file.Close()
 
 	// construct multi part
 	body := &bytes.Buffer{}
 	writer := multipart.NewWriter(body)
-	defer func() {
-		_ = writer.Close()
-	}()
 
 	part, err := writer.CreateFormFile("file", filepath.Base(logFile))
-	if err != nil {
+	if util.HasError(err) {
 		return err
 	}
 
 	_, err = io.Copy(part, file)
+	if util.HasError(err) {
+		return err
+	}
+
+	// flush file to writer
+	writer.Close()
 
 	// send request
 	url := config.Server + "/agents/logs/upload"
@@ -79,8 +87,25 @@ func uploadLog(cmd *domain.ExecutedCmd) error {
 	request.Header.Set(util.HttpHeaderAgentToken, config.Token)
 	request.Header.Set(util.HttpHeaderContentType, writer.FormDataContentType())
 
-	http.DefaultClient.Do(request)
-	//TODO: test
+	response, err := http.DefaultClient.Do(request)
+	if util.HasError(err) {
+		return err
+	}
+
+	// get response data
+	raw, _ := ioutil.ReadAll(response.Body)
+	var message domain.Response
+	err = json.Unmarshal(raw, &message)
+	if util.HasError(err) {
+		return err
+	}
+
+	if message.IsOk() {
+		util.LogDebug("Log for cmd '%s' has been uploaded", cmd.ID)
+		return nil
+	}
+
+	return fmt.Errorf(message.Message)
 }
 
 func writeLogToQueue(exchange string, qChannel *amqp.Channel, item *domain.LogItem) {
