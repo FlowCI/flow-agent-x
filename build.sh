654f5d3c524f7188e92d553910d42e9be941a1cd
gy2006 <32008001@qq.com>
2019-08-30 23:23:42 +0200

    add version to golang docker

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # build within golang docker
 docker run -it --rm \
 -v "$PWD":/go/src/flow-agent-x \
--w /go/src/flow-agent-x golang \
+-w /go/src/flow-agent-x golang:1.12 \
 /bin/bash -c "curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh && dep ensure && go build"
 
 docker build -f ./Dockerfile -t flowci/agent:latest .
