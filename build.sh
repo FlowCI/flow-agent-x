ead71b637988296050646a8715f901d96bfd224b
gy2006 <benqyang.2006.se@gmail.com>
2019-09-02 15:46:28 +0200

    support version tag on build script

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
+version=$1
+
+if [[ -n $version ]]; then
+  VersionTag="-t flowci/agent:$version"
+fi
+
 # build within golang docker
 docker run -it --rm \
 -v "$PWD":/go/src/flow-agent-x \
 -w /go/src/flow-agent-x golang:1.12 \
 /bin/bash -c "curl https://raw.githubusercontent.com/golang/dep/master/install.sh | sh && dep ensure && go build"
 
-docker build -f ./Dockerfile -t flowci/agent:latest .
+docker build -f ./Dockerfile -t flowci/agent:latest $VersionTag .
 
 # docker rmi -f $(docker images -f 'dangling=true' -q)
