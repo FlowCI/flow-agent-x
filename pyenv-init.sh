dae66f30ca990cfc76ebf1b1f99d482a7d993158
gy2006 <32008001@qq.com>
2020-04-09 01:51:51 +0200

    fix python path

--- pyenv-init.sh
+++ pyenv-init.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-docker pull flowci/pyenv:1.0
-docker create volumn pyenv
-docker run --rm -v pyenv:/ws pyenv1.0 ~/init-pyenv-volume.sh
+docker volume create pyenv
+docker run --rm -v pyenv:/ws flowci/pyenv:1.0 bash -c "~/init-pyenv-volume.sh"
 
-export FLOWCI_AGENT_VOLUMES="name=pyenv,dest=/ci/.pyenv,script=init.sh;$FLOWCI_AGENT_VOLUMES"
+export FLOWCI_AGENT_VOLUMES="name=pyenv,dest=/ci/python,script=init.sh;$FLOWCI_AGENT_VOLUMES"
