00ea4aa65c531cde77de4ed6d1b9be699c2f088a
gy2006 <32008001>
2020-10-06 16:40:34 +0200

    mv byte array to var

--- executor/util_win.go
+++ executor/util_win.go
@@ -12,6 +12,12 @@ import (
 	"strings"
 )
 
+var (
+	winUTF16Dash = []byte{0, 45, 0, 45, 0, 45, 0, 45} // ----
+	winUTF16CR   = []byte{0, 13}                      // \r
+	winNUL       = []byte{0}
+)
+
 func createCommand() (*exec.Cmd, error) {
 	path, err := exec.LookPath(winPowerShell)
 
@@ -31,23 +37,20 @@ func readEnvFromReader(r io.Reader, filters []string) domain.Variables {
 	output := domain.NewVariables()
 	process := false
 
-	utf16Dash := []byte{0, 45, 0, 45, 0, 45, 0, 45} // ----
-	utf16EndWith := []byte{0, 13}
-
 	for {
 		line, _, err := reader.ReadLine()
 		if err != nil {
 			return output
 		}
 
-		if util.IsByteStartWith(line, utf16Dash) {
+		if util.IsByteStartWith(line, winUTF16Dash) {
 			process = true
 			continue
 		}
 
 		if process {
-			line = util.BytesTrimLeft(line, []byte{0})
-			line = util.BytesTrimLeft(line, utf16EndWith)
+			line = util.BytesTrimLeft(line, winNUL)
+			line = util.BytesTrimLeft(line, winUTF16CR)
 			if len(line) == 0 {
 				continue
 			}
