ead7192c6767e35775dd862a7f95770909ea02d3
gy2006 <32008001@qq.com>
2021-12-25 21:30:27 +0100

    fix unable to fetach ssh-rsa secret

--- domain/secret_rsa.go
+++ domain/secret_rsa.go
@@ -16,5 +16,6 @@ type (
 func (s *RSASecret) ToEnvs() map[string]string {
 	return map[string]string{
 		s.GetName() + "_PUBLIC_KEY":  s.Pair.PublicKey,
+		s.GetName() + "_PRIVATE_KEY":  s.Pair.PrivateKey,
 	}
 }
