e94b89b4fac2c8cca9668e305ba25d6b57225f5b
gy2006 <32008001@qq.com>
2021-12-28 21:52:14 +0100

    fix output from smtp config

--- domain/config_smtp.go
+++ domain/config_smtp.go
@@ -1,17 +1,19 @@
 package domain
 
+import "strconv"
+
 type SmtpConfig struct {
 	ConfigBase
 	Server     string
 	Port       int
-	SecureType string
+	SecureType string `json:"secure"`
 	Auth       *SimpleAuthPair
 }
 
 func (c *SmtpConfig) ToEnvs() map[string]string {
 	return map[string]string{
 		c.GetName() + "_SERVER":        c.Server,
-		c.GetName() + "_PORT":          string(rune(c.Port)),
+		c.GetName() + "_PORT":          strconv.Itoa(c.Port),
 		c.GetName() + "_SECURE_TYPE":   c.SecureType,
 		c.GetName() + "_AUTH_USERNAME": c.Auth.Username,
 		c.GetName() + "_AUTH_PASSWORD": c.Auth.Password,
