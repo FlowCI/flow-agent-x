972bb05c501d433ce11664fc7bbdc21342769837
gy2006 <32008001@qq.com>
2022-01-19 19:02:22 +0100

    add idea run file

--- executor/executor.go
+++ executor/executor.go
@@ -88,17 +88,17 @@ type BaseExecutor struct {
 type Options struct {
 	K8s *domain.K8sConfig
 
-	AgentId                        string
-	Parent                         context.Context
+	AgentId                   string
+	Parent                    context.Context
 	Workspace                 string
 	WorkspaceFromDockerVolume bool
 	PluginDir                 string
-	CacheSrcDir                    string
-	Cmd                            *domain.ShellIn
-	Vars                           domain.Variables
-	SecretVars                     domain.Variables
-	ConfigVars                     domain.Variables
-	Volumes                        []*domain.DockerVolume
+	CacheSrcDir               string
+	Cmd                       *domain.ShellIn
+	Vars                      domain.Variables
+	SecretVars                domain.Variables
+	ConfigVars                domain.Variables
+	Volumes                   []*domain.DockerVolume
 }
 
 func NewExecutor(options Options) Executor {
